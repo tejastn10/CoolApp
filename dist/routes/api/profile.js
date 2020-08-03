8a8186d8c1baef8e50f68d22d2bdc3a59fa1e4eb
tejastn10 <tejastn10@gmail.com>
2020-08-03 16:05:23 +0530

    📦 UPDATE: User Profile route

--- dist/routes/api/profile.js
+++ dist/routes/api/profile.js
@@ -1,9 +1,35 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const auth_1 = __importDefault(require("../../middleware/auth"));
+const Profile_model_1 = require("../../models/Profile.model");
 const router = express_1.Router();
-// @route   Get api/profile
-// @desc    Test route
-// @access  Public
-router.get("/", (req, res) => res.send("Profile Route"));
+// @route   Get api/profile/me
+// @desc    Get current user's profile
+// @access  Private
+router.get("/me", auth_1.default, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const profile = yield Profile_model_1.Profile.findOne({ user: req.user.id }).populate("user", ["name", "avatar"]);
+        if (!profile) {
+            return res.status(400).json({ msg: "There is no Profile for this user!" });
+        }
+        res.json(profile);
+    }
+    catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error!");
+    }
+}));
 module.exports = router;
--- src/routes/api/profile.ts
+++ src/routes/api/profile.ts
@@ -1,10 +1,34 @@
 import { Router, Request, Response } from "express";
+import auth from "../../middleware/auth";
+import { Profile } from "../../models/Profile.model";
 
 const router: Router = Router();
 
-// @route   Get api/profile
-// @desc    Test route
-// @access  Public
-router.get("/", (req: Request, res: Response) => res.send("Profile Route"));
+// @route   Get api/profile/me
+// @desc    Get current user's profile
+// @access  Private
+router.get(
+  "/me",
+  auth,
+  async (req: Request, res: Response) => {
+    try {
+      const profile = await Profile.findOne({ user: req.user.id }).populate(
+        "user",
+        ["name", "avatar"],
+      );
+
+      if (!profile) {
+        return res.status(400).json(
+          { msg: "There is no Profile for this user!" },
+        );
+      }
+
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error!");
+    }
+  },
+);
 
 module.exports = router;
