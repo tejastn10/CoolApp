ae523625f0b977a41bfcd7b5d3101afef8671c97
Tejas Nikhar <dashboardtn10@pop-os.localdomain>
2020-08-26 16:04:25 +0530

    🐛 FIX: Navbar position

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -2,7 +2,7 @@ import React, { FC } from "react";
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
 
 export const Navbar: FC = () => (
-  <AppBar position="static">
+  <AppBar position="fixed">
     <Toolbar>
       <Typography variant="h6">∘ ○ 〇 Meet and Greet 〇 ○ ∘</Typography>
     </Toolbar>
