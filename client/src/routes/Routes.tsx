35d6a25a1a6a0661d6cbb208225bfe697aa1bb09
Tejas Nikhar <dashboardtn10@pop-os.localdomain>
2020-08-26 16:06:02 +0530

    💫 STYLE:  App body

--- client/src/routes/Routes.tsx
+++ client/src/routes/Routes.tsx
@@ -1,15 +1,30 @@
 import React, { FC } from "react";
 import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
 
 import { Navbar } from "../components/layout/Navbar";
+import { Landing } from "../components/pages/Landing";
 
-export const Routes: FC = () => (
-  <Grid container direction="column">
-    <Grid item>
-      <Navbar />
+const useStyles = makeStyles({
+  header: {
+    height: "10vh",
+  },
+  layout: {
+    height: "90vh",
+    padding: "2.5rem 3rem",
+  },
+});
+export const Routes: FC = () => {
+  const classes = useStyles();
+
+  return (
+    <Grid container direction="column">
+      <Grid item className={classes.header}>
+        <Navbar />
+      </Grid>
+      <Grid item container className={classes.layout}>
+        <Landing />
+      </Grid>
     </Grid>
-    <Grid item container>
-      Content
-    </Grid>
-  </Grid>
-);
+  );
+};
