5bb44033833275402cb4e6afdd9a8e0aa3279c5f
Tejas Nikhar <dashboardtn10@pop-os.localdomain>
2020-08-26 11:56:04 +0530

    📦 UPDATE: Frontend files

--- client/src/components/layout/Sidebar.tsx
+++ client/src/components/layout/Sidebar.tsx
@@ -1,13 +1,3 @@
 import React, { FC } from "react";
-import { Layout } from "antd";
-import styled from "styled-components";
 
-const { Sider } = Layout;
-
-const StyledSider = styled(Sider)`
-  background: #adadad;
-  min-height: 92vh;
-  min-width: 20vw;
-`;
-
-export const Sidebar: FC = () => <StyledSider>Sidebar</StyledSider>;
+export const Sidebar: FC = () => <section>Sidebar</section>;
