e6215524bbf59cacce153dabd322728459c7d414
Tejas Nikhar <tejastn10@gmail.com>
2020-11-10 20:55:30 +0530

    ️🏷 UPDATE: types file with AuthState

--- client/src/store/@types/types.d.ts
+++ client/src/store/@types/types.d.ts
@@ -4,6 +4,13 @@ export interface Alert {
   alertType: string;
 }
 
+export interface AuthState {
+  token: string | null;
+  isAuthenticated: boolean | null;
+  loading: boolean | null;
+  user: null;
+}
+
 export interface AlertState {
   alerts: Alert[] | null;
 }
