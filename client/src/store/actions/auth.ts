bc56a730bfbbcb1e6613c8cb4e9ce9efe00e45dd
Tejas Nikhar <tejastn10@gmail.com>
2020-11-11 22:32:46 +0530

    📦 UPDATE: Auth Actions file

--- client/src/store/actions/auth.ts
+++ client/src/store/actions/auth.ts
@@ -3,7 +3,18 @@ import {
   REGISTER_REQUEST,
   REGISTER_SUCCESS,
   REGISTER_FAIL,
+  AUTH_REQUEST,
+  AUTH_SUCCESS,
+  AUTH_ERROR,
 } from "./actionTypes";
+
+export const authRequest = createAction(AUTH_REQUEST);
+export const authSuccess = createAction(AUTH_SUCCESS, (data: any) => {
+  return {
+    payload: data,
+  };
+});
+export const authError = createAction(AUTH_ERROR);
 
 export const registerRequest = createAction(REGISTER_REQUEST, (data: any) => {
   return { payload: data };
