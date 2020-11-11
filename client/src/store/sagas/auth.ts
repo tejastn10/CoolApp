7aa0a3dfd6fb8e16aa4379e14d54cb525e05a088
Tejas Nikhar <tejastn10@gmail.com>
2020-11-11 22:36:29 +0530

    📦 UPDATE: Auth saga

--- client/src/store/sagas/auth.ts
+++ client/src/store/sagas/auth.ts
@@ -1,21 +1,50 @@
 import { Action, nanoid } from "@reduxjs/toolkit";
 import { all, call, delay, fork, put, takeLatest } from "redux-saga/effects";
-import { api } from "../../services/API";
+import { registerUser, userAuth } from "../../services/API";
 import { removeAlert, setAlert } from "../actions/alert";
 import {
   registerRequest,
   registerSuccess,
   registerFail,
+  authRequest,
+  authError,
+  authSuccess,
 } from "../actions/auth";
+
+function* auth(action: Action) {
+  try {
+    if (authRequest.match(action)) {
+      const res = yield call(userAuth, action.payload);
+      const data = res.data;
+      const id = nanoid();
+      if (res.status !== 200) {
+        localStorage.removeItem("token");
+        yield put(authError());
+        yield put(setAlert(id, "Authentication Failed! Server Error", "error"));
+        yield delay(5000);
+        yield put(removeAlert(id));
+      } else {
+        yield put(authSuccess(data));
+      }
+    }
+  } catch (err) {
+    const id = nanoid();
+    localStorage.removeItem("token");
+    yield put(authError());
+    yield put(setAlert(id, "Authorization Failed!!!", "error"));
+    yield delay(5000);
+    yield put(removeAlert(id));
+  }
+}
 
 function* register(action: Action) {
   try {
     if (registerRequest.match(action)) {
-      const res = yield call(api, action.payload);
+      const res = yield call(registerUser, action.payload);
       const data = res.data;
-      console.log(data);
       const id = nanoid();
       if (res.status !== 200) {
+        localStorage.removeItem("token");
         yield put(registerFail());
         yield put(setAlert(id, "Registration Failed! Server Error", "error"));
         yield delay(5000);
@@ -29,6 +58,7 @@ function* register(action: Action) {
     }
   } catch (err) {
     const id = nanoid();
+    localStorage.removeItem("token");
     yield put(registerFail());
     yield put(
       setAlert(
@@ -46,6 +76,10 @@ function* watchRegisterRequest() {
   yield takeLatest(registerRequest.type, register);
 }
 
+function* watchAuthRequest() {
+  yield takeLatest(authRequest.type, auth);
+}
+
 export default function* registerSaga() {
-  yield all([fork(watchRegisterRequest)]);
+  yield all([fork(watchRegisterRequest), fork(watchAuthRequest)]);
 }
