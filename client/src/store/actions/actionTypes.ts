52cb17e5f1dc1ef5483884bea80ee32d5d754d2c
Tejas Nikhar <tejastn10@gmail.com>
2020-11-11 22:33:14 +0530

    📦 UPDATE: Auth Action Types

--- client/src/store/actions/actionTypes.ts
+++ client/src/store/actions/actionTypes.ts
@@ -4,3 +4,7 @@ export const REMOVE_AlERT = "REMOVE_AlERT";
 export const REGISTER_REQUEST = "REGISTER_REQUEST";
 export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
 export const REGISTER_FAIL = "REGISTER_FAIL";
+
+export const AUTH_REQUEST = "AUTH_REQUEST";
+export const AUTH_SUCCESS = "AUTH_SUCCESS";
+export const AUTH_ERROR = "AUTH_ERROR";
