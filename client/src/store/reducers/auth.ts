ef6849d8bcac9faa0d534141f0f790bde5cf4523
Tejas Nikhar <tejastn10@gmail.com>
2020-11-11 22:33:43 +0530

    📦 UPDATE: Auth reducer

--- client/src/store/reducers/auth.ts
+++ client/src/store/reducers/auth.ts
@@ -4,6 +4,9 @@ import {
   registerRequest,
   registerSuccess,
   registerFail,
+  authRequest,
+  authSuccess,
+  authError,
 } from "../actions/actions";
 
 const initialState: AuthState = {
@@ -15,25 +18,27 @@ const initialState: AuthState = {
 
 const reducer = createReducer(initialState, (builder) => {
   return builder
+    .addCase(authRequest, (state) => {
+      state.loading = true;
+    })
+    .addCase(authSuccess, (state, action) => {
+      state.isAuthenticated = true;
+      state.loading = false;
+      state.user = action.payload;
+    })
     .addCase(registerRequest, (state) => {
-      state = { ...state };
+      state = state;
     })
     .addCase(registerSuccess, (state, action) => {
       localStorage.setItem("token", action.payload.token);
-      state = {
-        ...state,
-        ...action.payload,
-        isAuthenticated: true,
-        loading: false,
-      };
+      state.loading = false;
+      state.isAuthenticated = true;
+      state.user = null;
     })
-    .addCase(registerFail, (state) => {
-      state = {
-        ...state,
-        token: null,
-        isAuthenticated: false,
-        loading: false,
-      };
+    .addCase(registerFail || authError, (state) => {
+      state.token = null;
+      state.isAuthenticated = false;
+      state.loading = false;
     });
 });
 
