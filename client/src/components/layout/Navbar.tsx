f5a56d283c881ab6ec73caafdbd2cfd565875170
Tejas Nikhar <tejastn10@gmail.com>
2020-11-16 14:48:55 +0530

    📦 UPDATE: Navbar with Logout Button

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -1,17 +1,35 @@
 import React, { FC } from "react";
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, Button, Toolbar, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { ApplicationState } from "../../store/store";
+import { AuthState } from "../../store/@types/types";
+import { logout } from "../../store/actions/actions";
 
 const useStyles = makeStyles({
   link: {
     textDecoration: "none",
     color: "#fff",
   },
+  button: {
+    marginLeft: "auto",
+  },
 });
 
 export const Navbar: FC = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  const logoutUser = () => {
+    dispatch(logout());
+  };
+
+  const authState = useSelector<ApplicationState, AuthState>(
+    (s) => s.authState
+  );
+
+  const { isAuthenticated, loading } = authState;
 
   return (
     <AppBar position="fixed">
@@ -19,6 +37,23 @@ export const Navbar: FC = () => {
         <Link to="/" className={classes.link}>
           <Typography variant="h6">∘ ○ 〇 Meet and Greet 〇 ○ ∘</Typography>
         </Link>
+        {!loading && (
+          <>
+            {isAuthenticated ? (
+              <Button
+                className={classes.button}
+                variant="outlined"
+                color="secondary"
+                onClick={() => logoutUser()}
+              >
+                {" "}
+                Logout{" "}
+              </Button>
+            ) : (
+              <div></div>
+            )}
+          </>
+        )}
       </Toolbar>
     </AppBar>
   );
