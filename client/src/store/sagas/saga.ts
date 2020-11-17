8852bd74087a54d892be5bc09b63627d90133151
Tejas Nikhar <tejastn10@gmail.com>
2020-11-17 15:27:29 +0530

    📦 UPDATE: Root saga

--- client/src/store/sagas/saga.ts
+++ client/src/store/sagas/saga.ts
@@ -1,8 +1,9 @@
 import { all, fork } from "redux-saga/effects";
 import registerSaga from "./auth";
+import profileSaga from "./profile";
 
 function* rootSaga() {
-  yield all([fork(registerSaga)]);
+  yield all([fork(registerSaga), fork(profileSaga)]);
 }
 
 export { rootSaga };
