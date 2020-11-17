89fb9288ff6863c8de7584bf091813e1d1a11b88
Tejas Nikhar <tejastn10@gmail.com>
2020-11-17 15:26:48 +0530

    📦 UPDATE: Root reducer

--- client/src/store/reducers/reducer.ts
+++ client/src/store/reducers/reducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import { alertReducer } from "./alert";
 import { authReducer } from "./auth";
+import { profileReducer } from "./profile";
 
 const reducers = {
   alertState: alertReducer,
   authState: authReducer,
+  profileState: profileReducer,
 };
 
 const createRootReducer = () => {
