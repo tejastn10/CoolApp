7927210938e98e01fb48070ffabc36d9284cddee
Tejas Nikhar <tejastn10@gmail.com>
2020-12-02 15:38:52 +0530

    📦 UPDATE: Profile Actions file

--- client/src/store/actions/profile.ts
+++ client/src/store/actions/profile.ts
@@ -1,5 +1,10 @@
 import { createAction } from "@reduxjs/toolkit";
-import { PROFILE_ERROR, PROFILE_REQUEST, PROFILE_SUCCESS } from "./actionTypes";
+import {
+  PROFILE_ERROR,
+  PROFILE_REQUEST,
+  PROFILE_SUCCESS,
+  CLEAR_PROFILE,
+} from "./actionTypes";
 
 export const profileRequest = createAction(PROFILE_REQUEST);
 export const profileSuccess = createAction(PROFILE_SUCCESS, (data: any) => {
@@ -12,3 +17,4 @@ export const profileError = createAction(PROFILE_ERROR, (data: any) => {
     payload: data,
   };
 });
+export const clearProfile = createAction(CLEAR_PROFILE);
