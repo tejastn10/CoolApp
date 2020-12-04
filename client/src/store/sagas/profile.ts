f7b3ff7fef57c1fdd6be0c4a3c98938722eeebce
Tejas Nikhar <tejastn10@gmail.com>
2020-12-04 22:40:16 +0530

    📦 UPDATE: Profile saga

--- client/src/store/sagas/profile.ts
+++ client/src/store/sagas/profile.ts
@@ -1,19 +1,19 @@
 import { Action, nanoid } from "@reduxjs/toolkit";
 import { all, call, delay, fork, put, takeLatest } from "redux-saga/effects";
-import { getCurrentProfile } from "../../services/API";
+import { getCurrentProfile, userProfile } from "../../services/API";
 import {
   profileError,
   profileRequest,
   profileSuccess,
+  profile,
 } from "../actions/profile";
 import { removeAlert, setAlert } from "../actions/alert";
 
-function* profile(action: Action) {
+function* getProfile(action: Action) {
   try {
     if (profileRequest.match(action)) {
       const res = yield call(getCurrentProfile);
       const data = res.data;
-      console.log(data);
       const id = nanoid();
       if (res.status !== 200) {
         yield put(profileError(res.statustext));
@@ -33,10 +33,42 @@ function* profile(action: Action) {
   }
 }
 
-function* watchProfileRequest() {
-  yield takeLatest(profileRequest.type, profile);
+function* setProfile(action: Action) {
+  try {
+    if (profile.match(action)) {
+      const res = yield call(userProfile, action.payload);
+      const data = res.data;
+      console.log(data);
+      const id = nanoid();
+      if (res.status !== 200) {
+        yield put(profileError(res.statustext));
+        yield put(setAlert(id, "Server Error", "error"));
+        yield delay(5000);
+        yield put(removeAlert(id));
+      } else {
+        yield put(profileSuccess(data));
+        yield put(setAlert(id, "Profile Updated", "success"));
+        yield delay(5000);
+        yield put(removeAlert(id));
+      }
+    }
+  } catch (err) {
+    const id = nanoid();
+    yield put(profileError(err.response.statustext));
+    yield put(setAlert(id, err.response.statusText, "error"));
+    yield delay(5000);
+    yield put(removeAlert(id));
+  }
+}
+
+function* watchgetProfileRequest() {
+  yield takeLatest(profileRequest.type, getProfile);
+}
+
+function* watchsetProfileRequest() {
+  yield takeLatest(profile.type, setProfile);
 }
 
 export default function* profileSaga() {
-  yield all([fork(watchProfileRequest)]);
+  yield all([fork(watchgetProfileRequest), fork(watchsetProfileRequest)]);
 }
